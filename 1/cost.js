cb3572daabce8c035f21dd2088406fd3a52ae591
Odisssssey <Good_man5@mail.ru>
2017-03-05 01:56:20 +0300

    new
    new

---
+++ 1/cost.js
@@ -0,0 +1,4 @@
+var name = "Телепорт бытовой VZHIH-101", price = 10000;
+console.log(`В наличии имеется: ${name}`);
+console.log(`Стоимость товара ${price} Q`);
+console.log(`При покупке двуходновременно цена покупки составит ${price + price / 100 * 90} Q`);
