f8eccfbe07125cd806ef7b264aa56a9a07dad6cf
Odisssssey <Good_man5@mail.ru>
2017-03-07 20:28:00 +0300

    new
    new

--- 7/date.js
+++ 7/date.js
@@ -59,8 +59,23 @@ function delay(pos, sum, dat){
 }
 
 delay(positions[0], 7200, new Date(2030, 4 - 1, 10));
+delay(positions[1], 57000, new Date(2030, 4 - 1, 10));
 
-console.log(deferedPayments.length); // 1
-console.log(deferedPayments[0].producer.name); // Рязанский телепортостроительный завод
-console.log(deferedPayments[0].amount); // 7200
-console.log(deferedPayments[0].paymentDate); // Sat Apr 20 2030 00:00:00 GMT
+deferedPayments.forEach(function (id, i){
+  console.log(`${id.paymentDate.toLocaleDateString('ru-Ru')}: ${id.producer.name}, сумма ${id.amount} Q`);
+});
+
+/*--3--*/
+
+function loadCurrencyJSON() {
+  return '{"AUD":44.95,"AZN":33.73,"GBP":73.42,"AMD":0.12,"BYN":30.96,"BGN":32.01,"BRL":18.8,"HUF":0.2,"DKK":8.42,"USD":58.85,"EUR":62.68,"INR":0.88,"KZT":0.18,"CAD":44.74,"KGS":0.85,"CNY":8.55,"MDL":2.94,"NOK":7.02,"PLN":14.55,"RON":13.92,"ZZZ":79.91,"SGD":41.36,"TJS":7.43,"TRY":15.97,"TMT":16.84,"UZS":0.02,"UAH":2.16,"CZK":2.32,"SEK":6.6,"CHF":58.69,"ZAR":4.4,"KRW":0.05,"JPY":0.52}';
+}
+
+function convertCurrency(amount, from, to){
+  var a = JSON.parse(loadCurrencyJSON());
+  return parseFloat(amount / a[`${to}`] * a[`${from}`]).toFixed(2);
+}
+
+
+let price1 = convertCurrency(7000, 'ZZZ', 'USD');
+console.log(`Сумма ${price1} USD`);// Сумма 9505.01 USD
