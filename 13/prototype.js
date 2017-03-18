9e798ebfa27dec9d31b8eeb5c8c187e0e1c8d388
Odisssssey <Good_man5@mail.ru>
2017-03-19 01:16:21 +0300

    new
    new

--- 13/prototype.js
+++ 13/prototype.js
@@ -32,3 +32,32 @@ OrdersTeleportationPoint.prototype = {
 const point = new OrdersTeleportationPoint('Темная сторона Луны', 500, 200, 97);
 let distance = point.getDistance(100, -100, 33);
 console.log(`Расстояние до пункта «${point.title}» составит ${distance.toFixed(0)} единиц`);
+
+/*--2--*/
+
+function OrdersTeleportationPointLocator(points) {
+  const locator = Object.create(OrdersTeleportationPointLocator.prototype);
+  locator.points = points;
+  return locator;
+}
+
+OrdersTeleportationPointLocator.prototype = {
+  getClosest(x, y, z) {
+    let myId;
+    this.points.forEach(function (id, i){
+      if(i === 0){
+        myId = id;
+      }
+      if(myId.getDistance(x, y, z) > id.getDistance(x, y, z)) {
+        myId = id;
+      }
+    });
+    return myId;
+  }
+};
+
+const points = pointsInfo.map(point => new OrdersTeleportationPoint(point.title, ...point.coords));
+const locator = new OrdersTeleportationPointLocator(points);
+console.log(locator.title);
+const closestPoint = locator.getClosest(333, 294, 77);
+console.log(`Ближайший пункт телепортации заказов «${closestPoint.title}»`);
