3d9508a9103dd8f2b2d4759e45e099e1c74b9167
Odisssssey <Good_man5@mail.ru>
2017-03-18 15:22:15 +0300

    new
    new

---
+++ 12/find.js
@@ -0,0 +1,30 @@
+const clients = [{
+  name: 'Филип Фрай',
+  email: 'fray@mail.un',
+  isSubscribed: false,
+  orders: [ 11700, 1980, 450, 5500 ]
+}, {
+  name: 'Бендер Сгибатель Родригес',
+  email: 'bender.rodriges@rambler.un',
+  isSubscribed: true,
+  orders: [ 440, 226, 7650, 2990, 70 ]
+}, {
+  name: 'Доктор Джон Зоидберг',
+  email: 'zoidberg-md@list.un',
+  isSubscribed: true,
+  orders: [ 720 ]
+}];
+
+clients.findByName = function(name){
+  return clients.find(function (man) {
+    if (man.name == name) {
+      return man;
+    }
+  });
+};
+
+const clientOne = clients.findByName('Доктор Джон Зоидберг');
+console.log(clientOne.email); // zoidberg-md@list.un
+
+const clientTwo = clients.findByName('Люрр');
+console.log(typeof clientTwo); // undefined
